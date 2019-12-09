979d34c0e9826a06185b7e2fecbf0103a2028d36
denisbond07 <denisokok7@gmail.com>
2019-12-09 22:29:40 +0200

    add slider

---
+++ js/multiple-items.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.multiple-items').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: true,
+      dots: true,
+      slidesToShow: 3,
+      variableWidth: true,
+      arrows:true,
+      // prevArrow: "<img src='https://img/news/left arrow.png' class='prev' alt='1'>",
+      // nextArrow: "<img src='https://svgshare.com/i/6Gf.svg' class='next' alt='2'>",
+    });
+  });
