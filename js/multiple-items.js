bce64192492fe4a61757b6767d99245b2bc0fc93
denisbond07 <denisokok7@gmail.com>
2019-12-13 00:09:02 +0200

    contact end

--- js/multiple-items.js
+++ js/multiple-items.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.multiple-items').slick({
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 5000,
       arrows: true,
       dots: true,
       slidesToShow: 3,
